132e2c735b20797dfcc12afee8106f93cde682ed
cleitonrs <rscleitonrs@gmail.com>
2022-03-15 15:21:04 -0300

    info component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import About from './components/About';
+import Info from './components/Info';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Info />
+      <About />
     </div>
   );
 }
---
+++ src/components/Info.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='info__container'>
+      <img className='hero' src="" alt="" />
+      <h1>Laura Smith</h1>
+      <h3>Frontend Developer</h3>
+      <h4>laurasmith.website</h4>
+      <div className="button__container">
+        <button className='email__btn'>Email</button>
+        <button className='linkedin__btn'>LinkedIn</button>
+      </div>
+    </div>
+  )
+}
+
+export default Info
