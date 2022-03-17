91250ed256c5d7a2cef36a03ccfb86f943fb35e0
cleitonrs <rscleitonrs@gmail.com>
2022-03-17 09:20:17 -0300

    interests component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Info from './components/Info';
+import Interests from './components/Interests';
 
 function App() {
   return (
     <div className="App">
       <Info />
       <About />
+      <Interests />
     </div>
   );
 }
