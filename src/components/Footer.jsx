a0fd5f54b8dcc3d7d66b1c3dc2470445bf8c2bad
cleitonrs <rscleitonrs@gmail.com>
2022-03-17 14:44:31 -0300

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import twitter from '../img/twitter.svg'
+import facebook from '../img/facebook.svg'
+import instagram from '../img/instagram.svg'
+import github from '../img/github.svg'
 
 const Footer = () => {
   return (
     <div className="footer__container">
-      
+      <img className='twitter__icon' src={twitter} alt="twitter icon" />
+      <img src={facebook} alt="facebook icon" className="facebook__icon" />
+      <img src={instagram} alt="instagram icon" className="instagram__icon" />
+      <img src={github} alt="github icon" className="github__icon" />
     </div>
   )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Laura from '../img/Laura.png'
 import mail from '../img/mail.svg'
 import linkedin from '../img/linkedin.svg'
 
 const Info = () => {
   return (
     <div className='info__container'>
-      <img className='hero' src="" alt="" />
+      <img className='hero' src={Laura} alt="Laura photo" />
       <h1>Laura Smith</h1>
       <h3>Frontend Developer</h3>
       <h4>laurasmith.website</h4>
