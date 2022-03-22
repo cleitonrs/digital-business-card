a83e9e1025fa81487b0f5be06301c08b5af533f6
cleitonrs <rscleitonrs@gmail.com>
2022-03-22 10:06:26 -0300

    links add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ import github from '../img/github.svg'
 const Footer = () => {
   return (
     <div className="footer__container">
-      <img className='twitter__icon' src={twitter} alt="twitter icon" />
-      <img src={facebook} alt="facebook icon" className="facebook__icon" />
-      <img src={instagram} alt="instagram icon" className="instagram__icon" />
-      <img src={github} alt="github icon" className="github__icon" />
+      <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><img className='twitter__icon' src={twitter} alt="twitter icon" /></a>
+      <a href="https://www.facebook.com" target="_blank" rel="noreferrer"><img src={facebook} alt="facebook icon" className="facebook__icon" /></a>
+      <a href="https://www.instagram.com" target="_blank" rel="noreferrer"><img src={instagram} alt="instagram icon" className="instagram__icon" /></a>
+      <a href="https://www.github.com" target="_blank" rel="noreferrer"><img src={github} alt="github icon" className="github__icon" /></a>
     </div>
   )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -11,8 +11,8 @@ const Info = () => {
       <h3>Frontend Developer</h3>
       <h4>laurasmith.website</h4>
       <div className="button__container">
-        <button className='email__btn'> <img className='email__icon' src={mail} alt="envelope" /> Email</button>
-        <button className='linkedin__btn'> <img className='linkedin__icon' src={linkedin} alt="linkedin icon" /> LinkedIn</button>
+        <button className='email__btn'> <img className='email__icon' src={mail} alt="envelope" /> <a href="https://www.gmail.com" target="_blank" rel="noreferrer" >Email</a></button>
+        <button className='linkedin__btn'> <img className='linkedin__icon' src={linkedin} alt="linkedin icon" /> <a href="https://www.linkedin.com" target="_blank" rel='noreferrer'>LinkedIn</a></button>
       </div>
     </div>
   )
